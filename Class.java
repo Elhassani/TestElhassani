45bf3c60aec29708f3fc0f1d45686463a63fcfad
Elhassani <ayoubhassani@hotmail.fr>
2016-01-10 23:50:55 +0000

    Calculatrice
    J'ai ajouter 2 méthodes d'ajout et de soustraire

--- Class.java
+++ Class.java
@@ -1,5 +1,8 @@
 class main String(args [])
 {
+	personne p =new personne();
+	p.Ajouter(5,6);
+	p.Soustraire(6,4);
 	System.out.println("Salut");
 }
 class personne
@@ -10,4 +13,17 @@ class personne
 	{
 		return a+b;
 	}
+	public int Soustraire(int a,int b)
+	{
+		if(a>=b)
+		{
+			return a-b;
+		}
+		if(a<b)
+		{
+			return null;
+		}
+
+	}
+
 }
